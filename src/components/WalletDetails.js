75fb2e425ebfe04ae479ec9fa868a8adbc7fa4f0
Yash4900 <satra.y@somaiya.edu>
2022-06-03 23:51:32 +0530

    resolved web3 issue

--- src/components/WalletDetails.js
+++ src/components/WalletDetails.js
@@ -1,17 +1,14 @@
 import React, { useState } from 'react'
 import metamask from '../images/metamask-fox.svg';
 import ethlogo from '../images/eth_logo.svg';
-import web3 from '../contract/web3.js';
 
 function WalletDetails() {
-
-  const [address, setAddress] = useState(web3.eth.getAddress()[0]);
-
+  
   return (
     <div id="wallet" className="p-4">
       <img src={metamask} height="25vh" alt="metamask" />
       <div id="balance" className="w500 my-2">
-        <img src={ethlogo} height="20vh" alt="ethlogo" /> <span>{ address }</span> ETH
+        <img src={ethlogo} height="20vh" alt="ethlogo" /> <span>0.3456</span> ETH
       </div>
     </div>
   )
--- src/contract/web3.js
+++
@@ -1,5 +0,0 @@
-import Web3 from './web3';
-
-var web3 = new Web3('http://localhost:7545');
-
-export default web3;
